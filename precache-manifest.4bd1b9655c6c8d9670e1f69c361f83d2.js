42d11d1ed74df2bc6f00e4a5e95694565b8c09c9
Callum Scholes <hapymailer74@gmail.com>
2019-09-28 16:26:03 +1000

    Updates

---
+++ precache-manifest.4bd1b9655c6c8d9670e1f69c361f83d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f7cf33966662c698f5557938107f6f",
+    "url": "/chess-js/index.html"
+  },
+  {
+    "revision": "5c51387800585c3d3938",
+    "url": "/chess-js/static/css/main.ebb09a56.chunk.css"
+  },
+  {
+    "revision": "f2e961f5c36dcb181a0b",
+    "url": "/chess-js/static/js/2.bc08f2b2.chunk.js"
+  },
+  {
+    "revision": "5c51387800585c3d3938",
+    "url": "/chess-js/static/js/main.cadb3733.chunk.js"
+  },
+  {
+    "revision": "d7185824f19a04a932c3",
+    "url": "/chess-js/static/js/runtime~main.23995880.js"
+  }
+]);
